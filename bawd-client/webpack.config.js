554355d7b1274a6a93e17fc25f9f66621bb4be96
George <me@georgebutter.com>
2020-02-26 00:20:35 +1100

    :bug: fix react mounting to html template

--- bawd-client/webpack.config.js
+++ bawd-client/webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
   entry: './src/index.tsx',
   mode: NODE_ENV,
   devtool: 'source-map',
+  watch: NODE_ENV === 'development',
   module: {
     rules: [
       {
@@ -24,13 +25,15 @@ module.exports = {
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js',  '.jsx'],
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
   },
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
-    new HtmlWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
   ],
 };
